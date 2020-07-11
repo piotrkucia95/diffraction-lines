73e941328df9174decded93d6ce30571fbe06ffe
piotrkucia95 <piotrkucia95@gmail.com>
2020-07-11 15:13:06 +0200

    Diffraction intensities UI

--- static/js/main.js
+++ static/js/main.js
@@ -51,3 +51,40 @@ var clearPanel = function() {
     jQuery('#time').remove();
     jQuery('#results').remove();
 }
+
+window.onload = function () {
+    var chart = new CanvasJS.Chart("chartContainer", {            
+      title:{
+        text: "Natężenie linii dyfrakcyjnych w zależności od kąta θ"              
+      },
+
+      data: [  //array of dataSeries     
+      { //dataSeries - first quarter
+   /*** Change type "column" to "bar", "area", "line" or "pie"***/        
+       type: "column",
+       name: "First Quarter",
+       dataPoints: [
+       { label: "banana", y: 58 },
+       { label: "orange", y: 69 },
+       { label: "apple", y: 80 },                                    
+       { label: "mango", y: 74 },
+       { label: "grape", y: 64 }
+       ]
+     },
+     { //dataSeries - second quarter
+
+      type: "column",
+      name: "Second Quarter",                
+      dataPoints: [
+      { label: "banana", y: 63 },
+      { label: "orange", y: 73 },
+      { label: "apple", y: 88 },                                    
+      { label: "mango", y: 77 },
+      { label: "grape", y: 60 }
+      ]
+    }
+    ]
+  });
+
+    chart.render();
+  }
