ab6257421a615f4c4d76ec0a2d40d801c003a470
Piotr Kucia <p.kucia@polsource.com>
2020-08-10 10:59:13 +0200

    Azure App Service test

--- application.py
+++ application.py
@@ -13,7 +13,8 @@ math = Math()
 
 @app.route('/')
 def index():
-    return app.send_static_file('index.html')
+    return "Hello World!"
+    # return app.send_static_file('index.html')
 
 @app.route('/matrix-inverse/gauss/<order>')
 def matrix_inverse_gauss(order):
